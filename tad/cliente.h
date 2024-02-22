cb394503e6a3651b32569d2d59b6cba9c25ee1b9
Romulolevy <125319115+Romulolevy@users.noreply.github.com>
2024-02-22 17:53:18 -0300

    Update cliente.h

--- tad/cliente.h
+++ tad/cliente.h
@@ -2,13 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-// Struct que armazenara os dados dos clientes cadastrados.
-typedef struct {
-    int id;
+typedef struct clientes{
     char nome[50];
     char endereco[100];
-} Cliente;
+    int ID;
+}Cliente;
 
 // Função para ler e armazenar os dados cadastrados em um arquivo de texto. 
 Cliente* lerClientes(char *nomeArquivo, int *numClientes);
@@ -21,3 +19,8 @@ void shellSort(Cliente arr[], int n);
 
 // Função para comparar os dados.
 int compararClientes(const void *a, const void *b);
+
+// Função para entrada de dados
+void Menu(Cliente * clientes);
+
+// teste
