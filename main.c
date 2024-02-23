78eec84fc25f1ef14d69aa3292e8377cc41bdafb
Carlos Henrique <114544383+CarlosH6@users.noreply.github.com>
2024-02-22 22:57:26 -0300

    reformulacao

--- main.c
+++ main.c
@@ -1,29 +1,33 @@
-#include "./tad/cliente.c" // Código de compilação pelo terminal "gcc -o main -c main.c"
+#include "./tad/cliente.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+// código para rodar no shell
+// gcc -c main.c -o main.o
+// gcc -c ./tad/cliente.c -o cliente.o
+// gcc main.o cliente.o -o main
 
 int main() {
-    FILE *arquivo = fopen("clientes.txt", "at");
-    if (arquivo == NULL) {
-        printf("Erro ao abrir o arquivo clientes.txt");
-        return 1;
-    }
+  FILE *arquivo = fopen("clientes.txt", "at");
+  if (arquivo == NULL) {
+    printf("Erro ao abrir o arquivo clientes.txt\n");
+    return 1;
+  }
 
-    fclose(arquivo);
+  fclose(arquivo);
 
-    int numClientes;
-    Cliente *clientes = lerClientes("clientes.txt", &numClientes);
+  int numClientes;
+  Cliente *clientes = lerClientes("clientes.txt", &numClientes);
 
-    printf("Clientes antes da ordenacao:\n");
-    imprimirClientes(clientes, numClientes);
+  printf("Clientes antes da ordenação:\n");
+  imprimirClientes(clientes, numClientes);
 
-    shellSort(clientes, numClientes);
+  shellSort(clientes, numClientes);
 
-    printf("\nClientes ordenados:\n");
-    imprimirClientes(clientes, numClientes);
+  printf("\nClientes ordenados:\n");
+  imprimirClientes(clientes, numClientes);
 
-    free(clientes);
+  free(clientes);
 
-    return 0;
+  return 0;
 }
-
-
-// teste
