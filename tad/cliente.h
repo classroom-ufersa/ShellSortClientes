83be0264e4d482c0422e59b77a5d3c35a0578b70
Carlos Henrique <114544383+CarlosH6@users.noreply.github.com>
2024-02-22 22:57:07 -0300

    reformulacao

--- tad/cliente.h
+++ tad/cliente.h
@@ -1,26 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#ifndef CLIENTE_H
+#define CLIENTE_H
 
-typedef struct clientes{
-    char nome[50];
-    char endereco[100];
-    int ID;
-}Cliente;
+typedef struct clientes {
+  char nome[50];
+  char endereco[100];
+  int ID;
+} Cliente;
 
-// Função para ler e armazenar os dados cadastrados em um arquivo de texto. 
-Cliente* lerClientes(char *nomeArquivo, int *numClientes);
+// Função para entrada de dados
+void Menu(Cliente *clientes);
 
-// Função que ira imprimir os dados dos clientes na tela.
-void imprimirClientes(Cliente *clientes, int numClientes);
+// Função para ler e armazenar os dados cadastrados em um arquivo de texto.
+Cliente *lerClientes(char *nomeArquivo, int *numClientes);
 
 // Função para ordenar os dados.
 void shellSort(Cliente arr[], int n);
 
-// Função para comparar os dados.
-int compararClientes(const void *a, const void *b);
+// Função que irá imprimir os dados dos clientes na tela.
+void imprimirClientes(Cliente *clientes, int numClientes);
 
-// Função para entrada de dados
-void Menu(Cliente * clientes);
-
-// teste
+#endif
